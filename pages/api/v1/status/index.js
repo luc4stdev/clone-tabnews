5c1835ba954f3850d800182a6d75529dc06dcdac
luc4stdev <luc4stdev@gmail.com>
2025-08-25 23:33:35 -0300

    Database configuration and .env setup

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ chave: "isso é uma string de status" });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS result");
+  return response.status(200).json({ database: result.rows[0].result === 2 });
 }
 
 export default status;
